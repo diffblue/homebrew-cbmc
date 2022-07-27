5537d3b7e6d57c0fb57221ceeb0e76babb40e4cd
Fotis Koutoulakis <fotis.koutoulakis@gmail.com>
2022-07-27 10:30:51 +0100

    Refactor bottle transformation script to handle arbitrary versions

--- transform_binary.sh
+++ transform_binary.sh
@@ -2,6 +2,16 @@
 
 VERSION=$1
 
+if [ -z "${VERSION}" ]; then
+  echo >&2 "Fatal error: VERSION not set"
+  echo >&2 "Fatal error: Need to provide version, example: 5.62.0"
+  exit 2
+fi
+
+CBMC_STRING="CbmcAT"
+VERSION_PROCESSED="${VERSION//./}"
+FORMULA_VERSION="$CBMC_STRING$VERSION_PROCESSED"
+
 for TAG in "arm64_monterey" "arm64_big_sur" "monterey" "big_sur" "catalina" "x86_64_linux"
 do
     echo "Processing bottle for $VERSION -- $TAG"
@@ -17,10 +27,10 @@ do
 
         tar -xzf $BOTTLE_NAME
         mv cbmc cbmc@$1
-        sed -iu 's/class Cbmc/class CbmcAT5610/g' cbmc@$1/$1/.brew/cbmc.rb
-        tar czf cbmc-$1-$TAG.bottle.tar.gz cbmc@$1
+        sed -iu "s/class Cbmc/class $FORMULA_VERSION/g" cbmc@$1/$1/.brew/cbmc.rb
+        tar czf cbmc@$1-$1.$TAG.bottle.tar.gz cbmc@$1
         rm -rf cbmc@$1
-        SHA=$(shasum -a 256 cbmc-$1-$TAG.bottle.tar.gz)
+        SHA=$(shasum -a 256 cbmc@$1-$1.$TAG.bottle.tar.gz)
         echo "$SHA"
     elif [[ $OUTPUT =~ $pat2 ]]; then
         BOTTLE_NAME=${BASH_REMATCH[1]}
@@ -28,10 +38,10 @@ do
 
         tar -xzf $BOTTLE_NAME
         mv cbmc cbmc@$1
-        sed -iu 's/class Cbmc/class CbmcAT5610/g' cbmc@$1/$1/.brew/cbmc.rb
-        tar czf cbmc-$1-$TAG.bottle.tar.gz cbmc@$1
+        sed -iu "s/class Cbmc/class $FORMULA_VERSION/g" cbmc@$1/$1/.brew/cbmc.rb
+        tar czf cbmc@$1-$1.$TAG.bottle.tar.gz cbmc@$1
         rm -rf cbmc@$1
-        SHA=$(shasum -a 256 cbmc-$1-$TAG.bottle.tar.gz)
+        SHA=$(shasum -a 256 cbmc@$1-$1.$TAG.bottle.tar.gz)
         echo "$SHA"
     fi
 done
